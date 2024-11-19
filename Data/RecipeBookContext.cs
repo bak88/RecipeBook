0404f6dd3d5d08533ed1d27e6db0c82738034405
bak88 <dimbak1990@gmail.com>
2024-11-19 16:47:38 +0300

    creat RBContext

---
+++ Data/RecipeBookContext.cs
@@ -0,0 +1,28 @@
+﻿using FinalСertificationRecipeBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalСertificationRecipeBook.Data
+{
+    public class RecipeBookContext : DbContext
+    {
+        public RecipeBookContext(DbContextOptions<RecipeBookContext> options) : base(options) 
+        { 
+        }        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = NINTENDO; Initial Catalog = RecipeBook; Trusted_Connection=True; TrustServerCertificate=True")
+                          .UseLazyLoadingProxies()
+                          .LogTo(Console.WriteLine);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,18 +5,16 @@ namespace FinalCertificationRecipeBook
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+            var builder = WebApplication.CreateBuilder(args);           
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+          
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -26,7 +24,7 @@ namespace FinalCertificationRecipeBook
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
+            app.UseAuthentication();
 
             app.MapControllers();
 
